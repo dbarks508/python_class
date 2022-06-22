7fefcdcd253a2391310a13c1c3628c6ad4dacedd
Dylan Barks <91198979+dbarks508@users.noreply.github.com>
2022-06-22 17:59:38 -0500

    Add files via upload

---
+++ Mod3Lab.py
@@ -0,0 +1,28 @@
+class Vehicle():
+    def __init__(self, type):
+        self.type = type
+
+class Automobile(Vehicle):
+    def __init__(self, type, year, make, model, doors, roof):
+        super().__init__(type)
+        self.year = year
+        self.make = make
+        self.model = model
+        self.doors = doors
+        self.roof = roof
+    
+    def showDetails(self):
+        print(f'||| Vehicle Info |||\nType: {self.type}\nyear: {self.year}\nMake: {self.make}\nModel: {self.model}\nNumber of doors: {self.doors}\nStyle of roof: {self.roof}')
+
+myType = input('Vehicle Type: ') 
+myYear = input('Year: ')
+myMake = input('Make: ')
+myModel = input('Model: ')
+myDoors = input('Number of doors: ')
+myRoof = input('Style of roof(open/closed): ')
+#transport = Automobile(myType, myYear, myMake, myModel, myDoors, myRoof)
+#transport.showDetails()
+
+
+ 
+
